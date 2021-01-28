50ba629805069f68b91ec99d8096f08c92efe198
ihor.lapka <ihor.lapka@everymatrix.com>
2021-01-28 22:42:50 +0200

    refactored for mongodb

--- src/main/java/guru/springframework/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/services/RecipeServiceImpl.java
@@ -20,54 +20,54 @@ import java.util.Set;
 @Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
-
+    
     private final RecipeRepository recipeRepository;
     private final RecipeCommandToRecipe recipeCommandToRecipe;
     private final RecipeToRecipeCommand recipeToRecipeCommand;
-
+    
     public RecipeServiceImpl(RecipeRepository recipeRepository, RecipeCommandToRecipe recipeCommandToRecipe, RecipeToRecipeCommand recipeToRecipeCommand) {
         this.recipeRepository = recipeRepository;
         this.recipeCommandToRecipe = recipeCommandToRecipe;
         this.recipeToRecipeCommand = recipeToRecipeCommand;
     }
-
+    
     @Override
     public Set<Recipe> getRecipes() {
         log.debug("I'm in the service");
-
+        
         Set<Recipe> recipeSet = new HashSet<>();
         recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
         return recipeSet;
     }
-
+    
     @Override
     public Recipe findById(String id) {
-
+        
         Optional<Recipe> recipeOptional = recipeRepository.findById(id);
-
+        
         if (!recipeOptional.isPresent()) {
             throw new NotFoundException("Recipe Not Found. For ID value: " + id );
         }
-
+        
         return recipeOptional.get();
     }
-
+    
     @Override
     @Transactional
     public RecipeCommand findCommandById(String id) {
         return recipeToRecipeCommand.convert(findById(id));
     }
-
+    
     @Override
     @Transactional
     public RecipeCommand saveRecipeCommand(RecipeCommand command) {
         Recipe detachedRecipe = recipeCommandToRecipe.convert(command);
-
+        
         Recipe savedRecipe = recipeRepository.save(detachedRecipe);
         log.debug("Saved RecipeId:" + savedRecipe.getId());
         return recipeToRecipeCommand.convert(savedRecipe);
     }
-
+    
     @Override
     public void deleteById(String idToDelete) {
         recipeRepository.deleteById(idToDelete);
